bd755b76c746cfce56fc84f275bc642e20b2c017
Nirantak Raghav <nirantak.s.r@gmail.com>
2018-02-04 18:01:34 +0530

    Performance improvements - js, images

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,44 @@ const autoprefixer = require("gulp-autoprefixer");
 const cleancss = require("gulp-clean-css");
 const concat = require("gulp-concat");
 const rename = require("gulp-rename");
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
 
-const sassOptions = {
+const sass_Options = {
 	errLogToConsole: true,
 	outputStyle: "compressed"
 };
-const minifyCssOptions = {
+const cleancss_Options = {
 	debug: true
 };
+const htmlmin_Options = {
+	collapseWhitespace: true
+};
+const imagemin_Plugins = [
+	imagemin.gifsicle({ interlaced: true }),
+	imagemin.jpegtran({ progressive: true }),
+	imagemin.optipng({ optimizationLevel: 5 }),
+	imagemin.svgo({
+		plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
+	})
+];
+const imagemin_Options = {
+	verbose: true
+};
+
+//Optimize Images
+gulp.task("img-min", function() {
+	return gulp
+		.src("src/img/*")
+		.pipe(imagemin(imagemin_Plugins, imagemin_Options))
+		.pipe(gulp.dest("images/"));
+});
 
 // Minify CSS
 gulp.task("minify-css", function() {
 	return gulp
-		.src("assets/css/main.css")
-		.pipe(cleancss(minifyCssOptions))
+		.src("src/css/main.css")
+		.pipe(cleancss(cleancss_Options))
 		.pipe(rename({ suffix: ".min" }))
 		.pipe(gulp.dest("assets/css/"));
 });
@@ -34,19 +58,18 @@ gulp.task("merge-css", function() {
 // Uglify JS
 gulp.task("uglify-js", function() {
 	return gulp
-	.src("assets/js/main.js")
+	.src("src/js/main.js")
 	.pipe(uglify().on("error", function(e) {
 		console.log(e);
 	}))
 	.pipe(rename({ suffix: ".min" }))
-	.pipe(gulp.dest("assets/js"));
+	.pipe(gulp.dest("assets/js/"));
 });
 
 // Merge JS files
 gulp.task("merge-js", function() {
 	return gulp
 	.src([
-		"assets/js/jquery.min.js",
 		"assets/js/jquery.scrollex.min.js",
 		"assets/js/jquery.scrolly.min.js",
 		"assets/js/skel.min.js",
@@ -55,16 +78,25 @@ gulp.task("merge-js", function() {
 		"assets/js/main.min.js"
 	])
 	.pipe(concat("scripts.js", { newLine: ";" }))
-	.pipe(gulp.dest("assets/js"));
+	.pipe(gulp.dest("assets/js/"));
 });
 
+// gulp.task("minify-html", function() {
+// 	return gulp
+// 		.src("src/*.html")
+// 		.pipe(htmlmin(htmlmin_Options))
+// 		.pipe(gulp.dest("dist"));
+// });
+
+gulp.task("img", ["img-min"]);
 gulp.task("css", ["minify-css", "merge-css"]);
 gulp.task("js", ["uglify-js", "merge-js"]);
-gulp.task("default", ["minify-css", "uglify-js", "merge-css", "merge-js"]);
+gulp.task("default", ["minify-css", "uglify-js", "img-min", "merge-css", "merge-js"]);
 
 gulp.task("watch", function() {
-	gulp.watch("assets/css/main.css", ["minify-css"]);
+	gulp.watch("src/css/main.css", ["minify-css"]);
 	gulp.watch("assets/css/*.css", ["merge-css"]);
-	gulp.watch("assets/js/main.js", ["uglify-js"]);
+	gulp.watch("src/js/main.js", ["uglify-js"]);
 	gulp.watch("assets/js/*.js", ["merge-js"]);
+	gulp.watch("src/img/*", ["img-min"]);
 });
